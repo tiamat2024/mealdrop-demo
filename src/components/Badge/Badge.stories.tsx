8b3370d2ad08f678cd9394fd456cbb2cf5ed299b
tiamat2024 <ryan@chromatic.com>
2024-06-07 11:53:39 -0500

    Update Badge.stories.tsx

--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -17,5 +17,5 @@ const Template: StoryFn<typeof Badge> = (args) => <Badge {...args} />
 
 export const Default = Template.bind({})
 Default.args = {
-  text: 'Comfort food',
+  text: 'Comfort foob',
 }
